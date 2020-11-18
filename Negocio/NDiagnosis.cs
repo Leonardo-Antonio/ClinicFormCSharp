df47644c13aebd9ecd9e729d033b379cb8bb87d0
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-18 13:47:36 -0500

    controlador del diagnostico

---
+++ Negocio/NDiagnosis.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Datos;
+using Entidades;
+
+namespace Negocio
+{
+    public class NDiagnosis
+    {
+        private DDiagnosis _dDiagnosis = new DDiagnosis();
+
+        public string SaveDiagnosis(EDiagnosis eDiagnosis)
+        {
+            return _dDiagnosis.SaveDiagnosis(eDiagnosis);
+        }
+
+        public DataTable ListDiagnosisPatient(EPatient ePatient)
+        {
+            return _dDiagnosis.ListDiagnosis(ePatient);
+        }
+
+        public string Delete(int ID)
+        {
+            if (ID < 0)
+            {
+                return "The ID entered is invalid";
+            }
+            return _dDiagnosis.Delete(ID);
+        }
+
+        public string Update(EDiagnosis eDiagnosis)
+        {
+            return _dDiagnosis.Update(eDiagnosis);
+        }
+    }
+}
