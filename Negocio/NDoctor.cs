6fc1817f89554d916ed2d758e52563a15675c26c
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-18 13:49:20 -0500

    controlador de doctor

---
+++ Negocio/NDoctor.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using System.Runtime.Remoting.Messaging;
+using Datos;
+using Entidades;
+
+namespace Negocio
+{
+    public class NDoctor
+    {
+        private DDoctor _dDoctor = new DDoctor();
+
+        public DataTable Log_in(EDoctor eDoctor)
+        {
+            if (eDoctor.DNI.Length != 8)
+            {
+                throw errors.Error.DNIInvalid;
+            } 
+            return _dDoctor.LogIn(eDoctor);
+        }
+    }
+}
