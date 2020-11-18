4d1632c0489c9b8880d6b1bc535a511fc5639e8f
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-18 13:43:45 -0500

    manejo de los datos nulos en la bd

---
+++ Datos/Valid/Null.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Datos.Valid
+{
+    public class Null
+    {
+        public static DBNull String(string valid)
+        {
+            return string.IsNullOrEmpty(valid) ? DBNull.Value : null;
+        }
+    }
+}
