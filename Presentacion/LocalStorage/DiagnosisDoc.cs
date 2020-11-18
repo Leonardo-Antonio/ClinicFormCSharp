8aa2200ebfbede8bfa9b1861eebf9648da242737
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-18 13:46:26 -0500

    localstorage

---
+++ Presentacion/LocalStorage/DiagnosisDoc.cs
@@ -0,0 +1,11 @@
+﻿namespace Presentacion
+{
+    public class DiagnosisDoc
+    {
+        public static int ID;
+        public static string ReasonsForConsultation;
+        public static string CurrentIllness;
+        public static string PathologicalHistory;
+        public static int IDHC;
+    }
+}
---
+++ Presentacion/LocalStorage/LogIn.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentacion
+{
+    public class LogInDoc
+    {
+        public static bool User = false;
+        public static byte ID;
+        public static string FullName;
+        public static string DNI;
+    }
+}
