7db0fb74da214ce6121532f0cf580f39ddd559c2
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-18 13:48:19 -0500

    entidad doctor

---
+++ Entidades/EDoctor.cs
@@ -0,0 +1,14 @@
+﻿namespace Entidades
+{
+    public class EDoctor
+    {
+        public byte ID;
+        public string Name;
+        public string A_p ;
+        public string A_m;
+        public string DNI;
+        public string Password;
+        public byte IDHour;
+        public byte IDSpeciality;
+    }
+}
