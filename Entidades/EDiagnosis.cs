08b53f161e7602370f0ac558c75b771409095c20
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-11-18 13:47:07 -0500

    entidad diagnostico

---
+++ Entidades/EDiagnosis.cs
@@ -0,0 +1,12 @@
+﻿namespace Entidades
+{
+    public class EDiagnosis
+    {
+        public int ID;
+        public string ReasonsForConsultation;
+        public string CurrentIllness;
+        public string PathologicalHistory;
+        public int IDClinicHistory;
+        public byte IDDdoctor;
+    }
+}
